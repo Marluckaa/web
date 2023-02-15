cdcf1cc20352d29d4defc8292b632e311e744976
Marluckaa <104973251+Marluckaa@users.noreply.github.com>
2023-02-15 12:39:08 +0800

    Add files via upload

---
+++ cs21d215/from tkinter import W, Variable.py
@@ -0,0 +1,36 @@
+from tkinter import W, Variable
+
+
+print("hello python) 
+a = "Hello" 
+b = "Kvle"
+print(a, b)
+# integer 
+a = 312312312
+b = 300
+print (b * a)
+
+# statement -
+
+if a < b:
+    print('b is bigger
+
+    elif a==
+
+tem = '+
+a = 5
+b = 10 
+print(
+    
+)
+
+
+
+
+
+
+
+
+
+
+
