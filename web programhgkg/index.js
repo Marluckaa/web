04f134ffd92a6787930cb0e3ea639dba16863466
Marluckaa <104973251+Marluckaa@users.noreply.github.com>
2023-04-05 10:53:43 +0800

    Add files via upload

---
+++ web programhgkg/index.js
@@ -0,0 +1,58 @@
+/*let name = "maral";
+let age = 20;
+let isRed = false;
+let firstname = undefined;
+let selectColor = null;
+let person = {name:"maral", age:20}
+person["name"]="mike";
+person.name = "mya";
+
+
+console.log(name);
+console.log(person);
+
+let colors = ['red','blue','green'];
+colors[2] = 1;
+console.log(colors);*/
+
+
+/*function greet(name){
+    console.log("hello "+name);
+}
+greet("bold");*/
+
+
+/*function kwad(too){
+
+    console.log(too +"kwadrat is "+ too * too);
+}
+kwad("5");
+function kwad(too){
+    return too*too;
+}
+console.log(kwad(2));*/
+function test(){
+    let fname = document.getElementById("fname").value;
+    let lname = document.getElementById("lname").value;
+    let oron = document.getElementById("country").value;
+    
+
+    alert(fname + ":is your first name\n"+lname+ ":is your last name\n"+oron+":is your country");
+    document.getElementById("print").innerHTML =fname + ":is your first name\n"+lname+ ":is your last name\n"+oron+":is your country" ;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
